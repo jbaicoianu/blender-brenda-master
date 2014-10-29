8e7f6fedd9c2cdb9743683645164fc78c20282a6
James Baicoianu <james_github@baicoianu.com>
2014-10-29 20:27:08 +0000

    Merge existing tiles when starting

--- scripts/brenda/watch-and-combine.sh
+++ scripts/brenda/watch-and-combine.sh
@@ -4,14 +4,20 @@ JOBNAME=$1
 JOBDIR=frames/$JOBNAME/
 OUTFILE=$2
 
-echo -n "Creating new output file $OUTFILE: "
-convert -size 1920x1200 xc:white $OUTFILE
+echo -n "Creating new output file '$OUTFILE': "
+convert -size 1920x1200 xc:white "$OUTFILE"
 echo .
 
-inotifywait -m $JOBDIR -e close_write -e moved_to |
+if [ $(find "$JOBDIR" -type f |wc -l) -gt 0 ]; then
+	echo "Combining existing files: "
+	find "$JOBDIR" -type f -exec composite {} "$OUTFILE" "$OUTFILE" \; # -exec basename {}  \;
+	echo
+fi
+
+inotifywait -m "$JOBDIR" -e close_write -e moved_to |
 	while read path action file; do
 		echo -n "Combining $file: "
-		composite $path/$file $OUTFILE $OUTFILE
+		composite "$path/$file" "$OUTFILE" "$OUTFILE"
 		echo .
 	done
 
