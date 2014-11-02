98988e0cad99027de2d78fa9ee33d949bb464df9
bioid <bioshock@gmail.com>
2014-11-02 08:14:50 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ cd ..
 npm install
 ln -s `pwd`/render/ `pwd`/grafana/dist/app/panels/render
 cp `pwd`/config/config.grafana.js `pwd`/grafana/dist/config.js
-cp `pwd`/config/serverconfig.sample.js `pwd`/serverconfig.js
+cp `pwd`/config/serverconfig.sample.js `pwd`/config/serverconfig.js
 mkdir jobdata
