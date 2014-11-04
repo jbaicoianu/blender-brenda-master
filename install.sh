3596757c2293d8885249b04460df37b130e3db1b
bioid <bioshock@gmail.com>
2014-11-04 23:45:31 +0000

    update install script to add projects.json if it doesn't exist

--- install.sh
+++ install.sh
@@ -9,4 +9,6 @@ npm install
 ln -s `pwd`/render/ `pwd`/grafana/dist/app/panels/render
 cp `pwd`/config/config.grafana.js `pwd`/grafana/dist/config.js
 cp `pwd`/config/serverconfig.sample.js `pwd`/config/serverconfig.js
-mkdir jobdata
+if [ ! -f `pwd`/server/projects.json ]; then
+  echo "{}" > `pwd`/server/projects.json
+fi
