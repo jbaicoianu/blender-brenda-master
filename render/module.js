e32fef1c6afa881d0a2d241d7bceef03e8cb7d3f
James Baicoianu <james_github@baicoianu.com>
2014-11-08 07:25:09 +0000

    Removed broken scroll code

--- render/module.js
+++ render/module.js
@@ -150,8 +150,6 @@ define([
     });
     socket.on('stdout', function(data) {
       $scope.stdout = $scope.stdout.concat(data);
-      var el = document.getElementById('stdout');
-      el.scrollTop = el.scrollHeight;
     });
     socket.on('exit', function(data) {
       $scope.exitstate = $scope.exitstate.concat(data);
