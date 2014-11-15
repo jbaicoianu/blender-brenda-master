c56357bacb3d52a5e91d739d97224edcae832f5f
James Baicoianu <james_github@baicoianu.com>
2014-11-15 20:28:16 +0000

    Use spaces instead of tabs for logging

--- scripts/brenda/job-debug.sh
+++ scripts/brenda/job-debug.sh
@@ -2,7 +2,7 @@ debug_log() {
   DEBUGDATE=$(date --rfc-3339=seconds)
   DEBUGDIR=$(dirname $DEBUGFILE)
   [ ! -d "$DEBUGDIR" ] && mkdir -p "$DEBUGDIR"
-  DEBUGSTR="$DEBUGDATE\t$PROJECTNAME\t$JOBNAME\t$DEBUGNAME\t$@"
+  DEBUGSTR="$DEBUGDATE $PROJECTNAME $JOBNAME $DEBUGNAME $@"
   echo "$DEBUGSTR" >>$DEBUGFILE
   echo "$DEBUGSTR"
 }
